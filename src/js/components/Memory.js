46ea8ed56e126fac61385ecdbb0369bc0b22c44b
Ewa Brzuszczak <heh81@wp.pl>
2021-05-17 00:38:06 +0200

    Memory Component

---
+++ src/js/components/Memory.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import ButtonFetchCard from "./ButtonFetchCard";
+import MemoryCard from "./MemoryCards"
+
+
+const Memory = () => {
+
+    const [card, setCard] = useState(null)
+    const API = "data/db.json"
+
+    const handleCard = () => {
+        fetch(API)
+            .then(response => {
+                if (response.ok) {
+                    return response;
+                }
+                throw Error("Błąd!!!")
+            })
+            .then(response => response.json())
+            .then(data => {
+                /////
+
+                let arr = [];
+                while (arr.length < 4) {
+                    let r = Math.floor(Math.random() * 4) + 1;
+                    if (arr.indexOf(r) === -1) arr.push(r);
+                }
+                let arr2 = [];
+                while (arr2.length < 4) {
+                    let r = Math.floor(Math.random() * 4) + 1;
+                    if (arr2.indexOf(r) === -1) arr2.push(r);
+                }
+
+                let randomIndex = [...arr, ...arr2];
+
+                let pair = randomIndex.map(el => {
+                    return data.results[el - 1]
+                });
+                ////
+                // console.log(data);
+                //  setCard(data.results);
+                setCard(pair)
+            })
+            .catch(error => console.log(error));
+
+    }
+
+    return (
+
+        <>
+            <ButtonFetchCard click={handleCard} />
+            {card ? < MemoryCard card={card} /> : card}
+
+
+        </>)
+
+}
+
+export default Memory;
---
+++ src/js/components/MemoryCards.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+
+
+const MemoryCard = (props) => {
+    const [openedCard, setOpenedCard] = useState([]);
+    const [matched, setMatched] = useState([]);
+
+
+    const pairOfCard = [...props.card];
+
+    function flipCard(index) {
+        setOpenedCard((opened) => [...opened, index]);
+    }
+
+    //   
+    useEffect(() => {
+        if (openedCard < 2) return;
+
+        const firstMatched = pairOfCard[openedCard[0]];
+        const secondMatched = pairOfCard[openedCard[1]];
+
+        if (firstMatched === secondMatched) setMatched([...matched, firstMatched.index]);
+
+        if (openedCard.length === 2) setTimeout(() => setOpenedCard([]), 1000);
+        if (matched.length === 4) setTimeout(window.alert, 1000, "GRATULACJE!");
+        if (matched.length === 4) setTimeout(() => setMatched([]), 1500);
+        if (matched.length === 4) setTimeout(() => setOpenedCard([]), 1500);
+
+    }, [openedCard]);
+
+    console.log(matched);
+    console.log(openedCard);
+
+    return (
+
+        <>
+            <div className="memor">
+                <div className="memory container" >
+                    {pairOfCard.map((el, index) => {
+                        //lets flip the card
+
+                        let isFlipped = false;
+
+                        if (openedCard.includes(index)) isFlipped = true;
+                        if (matched.includes(el.index)) isFlipped = true;
+                        return (
+                            <div
+                                className={`memory-card ${isFlipped ? "flipped" : ""} `}
+                                key={index}
+                                onClick={() => flipCard(index)}
+                            >
+                                <div className="memory-inner">
+                                    <div className="memory-front">
+                                        <img
+                                            src={el.address}
+                                            alt={el.translate}
+                                            width="100"
+                                        />
+                                    </div>
+                                    <div className="memory-back"></div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default MemoryCard;
