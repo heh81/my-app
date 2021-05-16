87932370af49098d84e3637def3f1d14ab1f7364
Ewa Brzuszczak <heh81@wp.pl>
2021-05-17 00:39:59 +0200

    Button Component

---
+++ src/js/components/ButtonFetchCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ButtonFetchCard = props => {
+
+    return (
+        <button className="button" onClick={props.click}>Wylosuj kafelki</button>
+    )
+}
+
+export default ButtonFetchCard;
---
+++ src/js/components/ButtonFetchDictation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const ButtonFetchWord = props => {
+
+    return (
+        <button className="button" onClick={props.click}>Wylosuj słówko</button>
+    )
+}
+
+export default ButtonFetchWord;
